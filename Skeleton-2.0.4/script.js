ee7b7a4a247082f8d00f10e727ffa803e1fd2cd4
Kevin Lang <kevinl20@yahoo.com>
2023-01-02 17:07:21 -0800

    updated css

--- Skeleton-2.0.4/script.js
+++ Skeleton-2.0.4/script.js
@@ -14,6 +14,26 @@ fetch('https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/rando
 
 
 
+
+// function getSource(id){
+// $.ajax ({
+//     url:"https://api.spoonacular.com/recipes/"+id+"/information?apiKey=1f324b5b8bmsh466e2ff07149a7cp1e32d6jsn01419d3b8f86",
+//     success:function(res){
+//         document.getElementById("sourceLink").innerHTML=res.sourceUrl
+//         document.getElementById("sourceLink").href=res.sourceUrl
+//     }
+// });
+// }
+// function searchBtn(q){
+//     $.ajax({
+//         url:"https://api.spoonacular.com/recipes/search?apiKey=1f324b5b8bmsh466e2ff07149a7cp1e32d6jsn01419d3b8f86&number=1&query="+q,
+//         success:function(res){
+//             document.getElementById("output").innerHTML="<h1>"+res.results[0].title+"</h1><br><img> src='"+res.baseUri+res.results[0].image+"'width='400'/><br>read in "+res.results[0].readyInMinutes+" minutes"
+//             getSource(res.results[0].id)
+//         }
+//     })
+// }
+
 //cocktail api set up
 const options = {
 	method: 'GET',
@@ -29,8 +49,8 @@ fetch('https://the-cocktail-db.p.rapidapi.com/filter.php?i=Gin', options)
 	.catch(err => console.error(err));
 
     
-<script>
+{/* <script>
 function basicPopup(url) {
 popupWindow = window.open(url,'action','height=550,width=800,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes')
 }
-</script>
+</script> */}
